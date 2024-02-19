7d87249e2717a8573064e366e0f190561cdda7dc
Alison Rafael <arpfreitas@uem.br>
2024-02-19 15:50:03 -0300

    OpenAIEmbeddings key

--- src/app.py
+++ src/app.py
@@ -226,7 +226,7 @@ def get_vectorstore_from_url(url):
     document_chunks = text_splitter.split_documents(document)
     
     # create a vectorstore from the chunks
-    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
+    vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY))
 
     return vector_store
 
